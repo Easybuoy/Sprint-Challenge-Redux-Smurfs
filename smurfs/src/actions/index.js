8a78fff1277fba1a41dede52e21e95149d0c224d
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-06-21 10:43:42 +0100

    add ability to delete and update smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -7,17 +7,6 @@ export const DELETE_SMURF = "DELETE_SMURF";
 export const GET_SMURF = "GET_SMURF";
 export const UPDATE_SMURF = "UPDATE_SMURF";
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
-
 export const getSmurfs = () => dispatch => {
   axios
     .get("http://localhost:3333/smurfs")
--- smurfs/src/reducers/smurf.js
+++ smurfs/src/reducers/smurf.js
@@ -17,14 +17,6 @@ const INITIAL_STATE = {
   error: null
 };
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
-
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case GET_SMURFS:
